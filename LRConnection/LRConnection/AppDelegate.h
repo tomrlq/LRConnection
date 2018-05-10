7271fdf3855b15f9c3f1ee2195396c84e4b6cd40
Ruan Lingqi <tomrlq@foxmail.com>
2018-05-10 15:06:15 +0800

    initial commit

---
+++ LRConnection/LRConnection/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LRConnection
+//
+//  Created by Ruan Lingqi on 09/05/18.
+//  Copyright © 2018年 Suntoyo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
