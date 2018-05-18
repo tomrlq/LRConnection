f7736445e11907f5025de0727191bb440f4f0798
Ruan Lingqi <tomrlq@foxmail.com>
2018-05-18 14:33:07 +0800

    fix info

--- LRConnection/LRConnection/LRConnection.h
+++ LRConnection/LRConnection/LRConnection.h
@@ -3,7 +3,7 @@
 //  LRConnection
 //
 //  Created by Ruan Lingqi on 12/05/18.
-//  Copyright © 2018年 Suntoyo. All rights reserved.
+//  Copyright © 2018年 tomrlq. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
